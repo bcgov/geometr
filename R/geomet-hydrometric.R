23ba774898ae40bab4fac752a74f9f25c1e9ad8a
Sam Albers <sam.albers@gov.bc.ca>
2018-07-06 11:08:11 -0700

    change geo filename again

--- R/geomet-hydrometric.R
+++ R/geomet-hydrometric.R
@@ -7,31 +7,20 @@
 #'
 #' @export
 
-geo_stations <- function(STATION_NUMBER = NULL, as_spatial = FALSE){
+geomet_stations <- function(STATION_NUMBER = NULL, as_spatial = FALSE){
 
   is_there_internet()
 
-  cli <- crul::HttpClient$new(url = paste0(base_url,"/hydrometric-stations/items"))
-  # do a GET request
-  res <- cli$get(query = list(STATION_NUMBER = STATION_NUMBER))
+  cli <- geomet_client(met_param = "hydrometric-annual-statistics")
 
-  res$raise_for_status()
-  # parse response to plain text (JSON in this case) - most likely you'll
+  res <- cli$get(query = list(STATION_NUMBER = STATION_NUMBER))
 
   txt <- res$parse("UTF-8")
 
   if(as_spatial) return(sf::read_sf(txt))
 
-  if(!as_spatial){
-    # parse to json
-    parsed <- jsonlite::fromJSON(txt)
+  if(!as_spatial) tibble_parse(parsed_txt = txt)
 
-    lat_long_strings <- parsed[["features"]][["geometry"]][["coordinates"]][[1]]
-
-    cbind(parsed[["features"]][["properties"]],
-          LATITUDE = lat_long_strings[2],
-          LONGITUDE = lat_long_strings[1])
-  }
 }
 
 
